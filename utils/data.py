d1cbc9947e6c0a5d0031c7872bf69751c1498ff9
Ambitious <32821691+KhrylchenkoKirill@users.noreply.github.com>
2019-10-11 21:23:18 +0300

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -6,6 +6,7 @@ import os
 filenames = ['X1', 'X2', 'X3', 'Y']
 
 class Data:
+    ''' Training data loader. '''
     def __init__(self, path):
         for name in filenames:
             self.__dict__[name] = pd.read_csv(os.path.join(path, '{}.csv'.format(name)), index_col='id')
@@ -13,6 +14,7 @@ class Data:
 test_filenames = ['X1', 'X2', 'X3']
 
 class TestData:
+    ''' Test data loader. '''
     def __init__(self, path):
         for name in test_filenames:
             self.__dict__[name] = pd.read_csv(os.path.join(path, '{}.csv'.format(name)), index_col='id')
